6028ab523627c6871ccae4e62484c7140e7bcbdc
mnapo <napolimatiase@gmail.com>
2023-07-26 07:25:15 -0300

    done 3)

--- subdir/module_2.lua
+++ subdir/module_2.lua
@@ -1,11 +1,16 @@
-require "module_3.lua" --wrong! change package.path
+local M3 = require "./module_3"
 local M = {}
 
 M.sum = function (a, b)
     return a + b
 end
 
+M.lets_get_an_error = function (something)
+    return M3.handle_errors(something)
+end
+
 M.num = 9
 M.str = "sup"
 M.arr = {4, ["word"] = "car"}
+
 return M
